e6f076786173da496aa87ecdcc7099d59809e2b5
Sai Teja Pratap <pts@avah.dev>
2019-06-23 20:25:40 -0700

    Fix typos; rearrage code; more typehints

--- kaggle:jigsaw-unintended-bias-in-toxicity-classification/simple_model.py
+++ kaggle:jigsaw-unintended-bias-in-toxicity-classification/simple_model.py
@@ -12,6 +12,18 @@ from typing import List, Tuple, Dict, Callable
 import tensorflow as tf
 import datetime
 
+# Flags.
+LSTM_UNITS: int = 128
+DENSE_HIDDEN_UNITS: int = 4 * LSTM_UNITS
+MAX_SEQUENCE_LENGTH: int = 200
+USE_CATEGORY_COLS: bool = True
+TESTING_MODE: bool = False
+ENABLE_TEXT_PROCESSING: bool = True
+BATCH_SIZE: int = 1024
+NUM_EPOCHS: int = 20
+TOKENIZER_NUM_WORDS: int = 50000
+
+# Facts.
 CATEGORY_COLS: List[str] = ['severe_toxicity', 'obscene', 'identity_attack',
                             'insult', 'threat']
 IDENTITY_COLS: List[str] = [
@@ -26,15 +38,7 @@ TEST_FILE: str = \
 EMBEDDING_FILES: List[str] = [
     '../input/fasttext-crawl-300d-2m/crawl-300d-2M.vec',
     '../input/glove840b300dtxt/glove.840B.300d.txt']
-
-LSTM_UNITS: int = 128
-DENSE_HIDDEN_UNITS: int = 4 * LSTM_UNITS
-MAS_SEQUENCE_LENGTH: int = 200
-USE_CATEGORY_COLS: int = False
-TESTING_MODE: bool = False
-ENABLE_TEXT_PROCESSING: bool = False
-BATCH_SIZE: int = 1024
-NUM_EPOCHS: int = 20
+# Data + Facts
 # TEXT PROCESSING STUFF (NOT SURE IF THIS HELPS)
 # TODO(dotslash): Cleanup this mapping.
 CONTRACTION_MAPPING: Dict[str, str] = {
@@ -153,10 +157,8 @@ CONTRACTION_MAPPING: Dict[str, str] = {
     "you're": "you are",
     "you've": "you have",
 }
-
 for k, v in list(CONTRACTION_MAPPING.items()):
     CONTRACTION_MAPPING[k.capitalize()] = v.capitalize()
-
 QUOTES = ['’', '‘', '´', '`']
 __SYMBOLS_TO_ISOLATE = '.,?!-;*"…:—()%#$&_/@＼・ω+=”“[]^–>\\°<~•≠™ˈʊɒ∞§{}·τα❤☺ɡ|¢→̶`❥━┣┫┗Ｏ►★©―ɪ✔®\x96\x92●£♥➤´¹☕≈÷♡◐║▬′ɔː€۩۞†μ✒➥═☆ˌ◄½ʻπδηλσερνʃ✬ＳＵＰＥＲＩＴ☻±♍µº¾✓◾؟．⬅℅»Вав❣⋅¿¬♫ＣＭβ█▓▒░⇒⭐›¡₂₃❧▰▔◞▀▂▃▄▅▆▇↙γ̄″☹➡«φ⅓„✋：¥̲̅́∙‛◇✏▷❓❗¶˚˙）сиʿ✨。ɑ\x80◕！％¯−ﬂﬁ₁²ʌ¼⁴⁄₄⌠♭✘╪▶☭✭♪☔☠♂☃☎✈✌✰❆☙○‣⚓年∎ℒ▪▙☏⅛ｃａｓǀ℮¸ｗ‚∼‖ℳ❄←☼⋆ʒ⊂、⅔¨͡๏⚾⚽Φ×θ￦？（℃⏩☮⚠月✊❌⭕▸■⇌☐☑⚡☄ǫ╭∩╮，例＞ʕɐ̣Δ₀✞┈╱╲▏▕┃╰▊▋╯┳┊≥☒↑☝ɹ✅☛♩☞ＡＪＢ◔◡↓♀⬆̱ℏ\x91⠀ˤ╚↺⇤∏✾◦♬³の｜／∵∴√Ω¤☜▲↳▫‿⬇✧ｏｖｍ－２０８＇‰≤∕ˆ⚜☁'
 __SYMBOLS_TO_REMOVE = '\n🍕\r🐵\xa0\ue014\t\uf818\uf04a\xad😢🐶️\uf0e0😜😎👊\u200b\u200e😁عدويهصقأناخلىبمغر😍💖💵Е👎😀😂\u202a\u202c🔥😄🏻💥ᴍʏʀᴇɴᴅᴏᴀᴋʜᴜʟᴛᴄᴘʙғᴊᴡɢ😋👏שלוםבי😱‼\x81エンジ故障\u2009🚌ᴵ͞🌟😊😳😧🙀😐😕\u200f👍😮😃😘אעכח💩💯⛽🚄🏼ஜ😖ᴠ🚲‐😟😈💪🙏🎯🌹😇💔😡\x7f👌ἐὶήιὲκἀίῃἴξ🙄Ｈ😠\ufeff\u2028😉😤⛺🙂\u3000تحكسة👮💙فزط😏🍾🎉😞\u2008🏾😅😭👻😥😔😓🏽🎆🍻🍽🎶🌺🤔😪\x08‑🐰🐇🐱🙆😨🙃💕𝘊𝘦𝘳𝘢𝘵𝘰𝘤𝘺𝘴𝘪𝘧𝘮𝘣💗💚地獄谷улкнПоАН🐾🐕😆ה🔗🚽歌舞伎🙈😴🏿🤗🇺🇸мυтѕ⤵🏆🎃😩\u200a🌠🐟💫💰💎эпрд\x95🖐🙅⛲🍰🤐👆🙌\u2002💛🙁👀🙊🙉\u2004ˢᵒʳʸᴼᴷᴺʷᵗʰᵉᵘ\x13🚬🤓\ue602😵άοόςέὸתמדףנרךצט😒͝🆕👅👥👄🔄🔤👉👤👶👲🔛🎓\uf0b7\uf04c\x9f\x10成都😣⏺😌🤑🌏😯ех😲Ἰᾶὁ💞🚓🔔📚🏀👐\u202d💤🍇\ue613小土豆🏡❔⁉\u202f👠》कर्मा🇹🇼🌸蔡英文🌞🎲レクサス😛外国人关系Сб💋💀🎄💜🤢َِьыгя不是\x9c\x9d🗑\u2005💃📣👿༼つ༽😰ḷЗз▱ц￼🤣卖温哥华议会下降你失去所有的钱加拿大坏税骗子🐝ツ🎅\x85🍺آإشء🎵🌎͟ἔ油别克🤡🤥😬🤧й\u2003🚀🤴ʲшчИОРФДЯМюж😝🖑ὐύύ特殊作戦群щ💨圆明园קℐ🏈😺🌍⏏ệ🍔🐮🍁🍆🍑🌮🌯🤦\u200d𝓒𝓲𝓿𝓵안영하세요ЖљКћ🍀😫🤤ῦ我出生在了可以说普通话汉语好极🎼🕺🍸🥂🗽🎇🎊🆘🤠👩🖒🚪天一家⚲\u2006⚭⚆⬭⬯⏖新✀╌🇫🇷🇩🇪🇮🇬🇧😷🇨🇦ХШ🌐\x1f杀鸡给猴看ʁ𝗪𝗵𝗲𝗻𝘆𝗼𝘂𝗿𝗮𝗹𝗶𝘇𝗯𝘁𝗰𝘀𝘅𝗽𝘄𝗱📺ϖ\u2000үսᴦᎥһͺ\u2007հ\u2001ɩｙｅ൦ｌƽｈ𝐓𝐡𝐞𝐫𝐮𝐝𝐚𝐃𝐜𝐩𝐭𝐢𝐨𝐧Ƅᴨןᑯ໐ΤᏧ௦Іᴑ܁𝐬𝐰𝐲𝐛𝐦𝐯𝐑𝐙𝐣𝐇𝐂𝐘𝟎ԜТᗞ౦〔Ꭻ𝐳𝐔𝐱𝟔𝟓𝐅🐋ﬃ💘💓ё𝘥𝘯𝘶💐🌋🌄🌅𝙬𝙖𝙨𝙤𝙣𝙡𝙮𝙘𝙠𝙚𝙙𝙜𝙧𝙥𝙩𝙪𝙗𝙞𝙝𝙛👺🐷ℋ𝐀𝐥𝐪🚶𝙢Ἱ🤘ͦ💸ج패티Ｗ𝙇ᵻ👂👃ɜ🎫\uf0a7БУі🚢🚂ગુજરાતીῆ🏃𝓬𝓻𝓴𝓮𝓽𝓼☘﴾̯﴿₽\ue807𝑻𝒆𝒍𝒕𝒉𝒓𝒖𝒂𝒏𝒅𝒔𝒎𝒗𝒊👽😙\u200cЛ‒🎾👹⎌🏒⛸公寓养宠物吗🏄🐀🚑🤷操美𝒑𝒚𝒐𝑴🤙🐒欢迎来到阿拉斯ספ𝙫🐈𝒌𝙊𝙭𝙆𝙋𝙍𝘼𝙅ﷻ🦄巨收赢得白鬼愤怒要买额ẽ🚗🐳𝟏𝐟𝟖𝟑𝟕𝒄𝟗𝐠𝙄𝙃👇锟斤拷𝗢𝟳𝟱𝟬⦁マルハニチロ株式社⛷한국어ㄸㅓ니͜ʖ𝘿𝙔₵𝒩ℯ𝒾𝓁𝒶𝓉𝓇𝓊𝓃𝓈𝓅ℴ𝒻𝒽𝓀𝓌𝒸𝓎𝙏ζ𝙟𝘃𝗺𝟮𝟭𝟯𝟲👋🦊多伦🐽🎻🎹⛓🏹🍷🦆为和中友谊祝贺与其想象对法如直接问用自己猜本传教士没积唯认识基督徒曾经让相信耶稣复活死怪他但当们聊些政治题时候战胜因圣把全堂结婚孩恐惧且栗谓这样还♾🎸🤕🤒⛑🎁批判检讨🏝🦁🙋😶쥐스탱트뤼도석유가격인상이경제황을렵게만들지않록잘관리해야합다캐나에서대마초와화약금의품런성분갈때는반드시허된사용🔫👁凸ὰ💲🗯𝙈Ἄ𝒇𝒈𝒘𝒃𝑬𝑶𝕾𝖙𝖗𝖆𝖎𝖌𝖍𝖕𝖊𝖔𝖑𝖉𝖓𝖐𝖜𝖞𝖚𝖇𝕿𝖘𝖄𝖛𝖒𝖋𝖂𝕴𝖟𝖈𝕸👑🚿💡知彼百\uf005𝙀𝒛𝑲𝑳𝑾𝒋𝟒😦𝙒𝘾𝘽🏐𝘩𝘨ὼṑ𝑱𝑹𝑫𝑵𝑪🇰🇵👾ᓇᒧᔭᐃᐧᐦᑳᐨᓃᓂᑲᐸᑭᑎᓀᐣ🐄🎈🔨🐎🤞🐸💟🎰🌝🛳点击查版🍭𝑥𝑦𝑧ＮＧ👣\uf020っ🏉ф💭🎥Ξ🐴👨🤳🦍\x0b🍩𝑯𝒒😗𝟐🏂👳🍗🕉🐲چی𝑮𝗕𝗴🍒ꜥⲣⲏ🐑⏰鉄リ事件ї💊「」\uf203\uf09a\uf222\ue608\uf202\uf099\uf469\ue607\uf410\ue600燻製シ虚偽屁理屈Г𝑩𝑰𝒀𝑺🌤𝗳𝗜𝗙𝗦𝗧🍊ὺἈἡχῖΛ⤏🇳𝒙ψՁմեռայինրւդձ冬至ὀ𝒁🔹🤚🍎𝑷🐂💅𝘬𝘱𝘸𝘷𝘐𝘭𝘓𝘖𝘹𝘲𝘫کΒώ💢ΜΟΝΑΕ🇱♲𝝈↴💒⊘Ȼ🚴🖕🖤🥘📍👈➕🚫🎨🌑🐻𝐎𝐍𝐊𝑭🤖🎎😼🕷ｇｒｎｔｉｄｕｆｂｋ𝟰🇴🇭🇻🇲𝗞𝗭𝗘𝗤👼📉🍟🍦🌈🔭《🐊🐍\uf10aლڡ🐦\U0001f92f\U0001f92a🐡💳ἱ🙇𝗸𝗟𝗠𝗷🥜さようなら🔼'
@@ -251,10 +253,10 @@ def binary_accuracy(y_true: tf.Tensor, y_pred: tf.Tensor):
 
 
 def build_model(embedding_matrix: np.array, num_other_results: int):
-    inp = Input(shape=(MAS_SEQUENCE_LENGTH,))
+    inp = Input(shape=(MAX_SEQUENCE_LENGTH,))
     x = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1],
                   embeddings_initializer=Constant(embedding_matrix),
-                  input_length=MAS_SEQUENCE_LENGTH,
+                  input_length=MAX_SEQUENCE_LENGTH,
                   trainable=False)(inp)
     x = SpatialDropout1D(0.2)(x)
     x = Bidirectional(CuDNNLSTM(LSTM_UNITS, return_sequences=True))(x)
@@ -282,7 +284,7 @@ def print_diff(s1: pandas.Series, s2: pandas.Series) -> None:
 
 
 # TODO(dotslash): Create a container type for return value of this
-# function.
+#                 function.
 def load_train_data() -> Tuple[np.array, np.array,
                                np.array, np.array,
                                pandas.DataFrame, pandas.DataFrame,
@@ -293,8 +295,12 @@ def load_train_data() -> Tuple[np.array, np.array,
     np.random.shuffle(train_data.values)
     logger.log('Loaded train and test data.')
 
-    # eliminate contractions
+    if TESTING_MODE:
+        train_data = train_data.head(10000)
+        test_data = test_data.head(10000)
+
     if ENABLE_TEXT_PROCESSING:
+        # eliminate contractions
         logger.log('Processing text.')
         cleaner = TextCleaner()
         orig: pandas.Series = train_data['comment_text'].copy(deep=True)
@@ -311,10 +317,6 @@ def load_train_data() -> Tuple[np.array, np.array,
         print_diff(train_data['comment_text'], orig)
         logger.log('Processed text.')
 
-    if TESTING_MODE:
-        train_data = train_data.head(10000)
-        test_data = test_data.head(10000)
-
     # Trim the train data and keep only the useful columns.
     useful_cols: List[str] = \
         ['id', 'comment_text', 'target'] + CATEGORY_COLS + IDENTITY_COLS
@@ -323,7 +325,7 @@ def load_train_data() -> Tuple[np.array, np.array,
     print('Sample test data\n' + test_data.head().to_string())
 
     # Create a tokenizer based on train and test data.
-    tokenizer: text.Tokenizer = text.Tokenizer(num_words=50000)
+    tokenizer: text.Tokenizer = text.Tokenizer(num_words=TOKENIZER_NUM_WORDS)
     tokenizer.fit_on_texts(list(train_data['comment_text']) + \
                            list(test_data['comment_text']))
     logger.log('Fit text tokens.')
@@ -331,9 +333,9 @@ def load_train_data() -> Tuple[np.array, np.array,
     # Prepare X, Y for training and testing.
     # We will convert the text to a sequence using the tokenizer.
     train_seq = tokenizer.texts_to_sequences(list(train_data['comment_text']))
-    train_seq = sequence.pad_sequences(train_seq, maxlen=MAS_SEQUENCE_LENGTH)
+    train_seq = sequence.pad_sequences(train_seq, maxlen=MAX_SEQUENCE_LENGTH)
     test_seq = tokenizer.texts_to_sequences(list(test_data['comment_text']))
-    test_seq = sequence.pad_sequences(test_seq, maxlen=MAS_SEQUENCE_LENGTH)
+    test_seq = sequence.pad_sequences(test_seq, maxlen=MAX_SEQUENCE_LENGTH)
     logger.log('Converted tokens to sequences.')
 
     x_train, y_train, y_other_train = \
@@ -348,9 +350,9 @@ def construct_embedding_matrix(tokenizer: text.Tokenizer) -> np.array:
     embeddings = [EmbeddingStore(embedding_file)
                   for embedding_file in EMBEDDING_FILES]
     # Construct a embedding matrix used for Embedding layer.
-    EMBEDDING_DIM = sum(embedding.vector_length for embedding in embeddings)
+    embedding_dim = sum(embedding.vector_length for embedding in embeddings)
     tokenizer_words = get_top_words(tokenizer)
-    embedding_matrix = np.zeros((len(tokenizer_words) + 1, EMBEDDING_DIM))
+    embedding_matrix = np.zeros((len(tokenizer_words) + 1, embedding_dim))
     for word, ind in tokenizer_words:
         embedding_matrix[ind] = np.concatenate(
             [embedding.embedding(word) for embedding in embeddings])
@@ -359,22 +361,26 @@ def construct_embedding_matrix(tokenizer: text.Tokenizer) -> np.array:
 
 
 def main():
-    x_train, y_train, y_other_train, x_test, \
-    train_data, test_data, tokenizer = load_train_data()
+    x_train, y_train, y_other_train, x_test, train_data, \
+        test_data, tokenizer = load_train_data()
     embedding_matrix = construct_embedding_matrix(tokenizer)
     sample_weights: pandas.Series = pandas.Series(
         data=np.ones(len(x_train), dtype=np.float32))
 
-    for column in IDENTITY_COLS + ['target']:
+    for column in IDENTITY_COLS:
         train_data[column] = np.where(train_data[column] >= 0.5, True, False)
-    train_id_columns_sum = train_data[IDENTITY_COLS].sum(axis=1)
-    inv_train_id_columns_sum = (~train_data[IDENTITY_COLS]).sum(axis=1)
+    bool_target: pandas.Series = pandas.Series(
+        data=np.where(train_data['target'] > 0.5, True, False))
+    inv_bool_target: pandas.Series = ~bool_target
+    train_id_columns_sum: pandas.Series = train_data[IDENTITY_COLS].sum(axis=1)
+    inv_train_id_columns_sum: pandas.Series = (~train_data[IDENTITY_COLS]).sum(
+        axis=1)
     # Focus more on the rows that have identity columns.
     sample_weights += train_id_columns_sum
     # Focus more on the false negatives
-    sample_weights += train_data['target'] * (inv_train_id_columns_sum)
+    sample_weights += (bool_target * inv_train_id_columns_sum)
     # Focus a lot more on the false positives
-    sample_weights += (~train_data['target']) * (train_id_columns_sum * 5)
+    sample_weights += ((inv_bool_target * train_id_columns_sum) * 5)
     sample_weights /= sample_weights.mean()
 
     # Build Model.
